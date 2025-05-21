600dbadeb0fe9f1339a50c0575d3d944cf115c90
Deepak Yadav <deepakyadav@Deepaks-MacBook-Pro.local>
2025-05-21 10:46:25 +0530

    deployment

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import react from "@vitejs/plugin-react";
 import svgr from "vite-plugin-svgr";
 
 export default defineConfig({
-  base: '/aspire-card/',
+  base: "./",
   plugins: [
     react(),
     svgr({
@@ -21,9 +21,14 @@ export default defineConfig({
   },
   build: {
     sourcemap: true,
+    outDir: "dist",
+    assetsDir: "assets",
     rollupOptions: {
       output: {
         manualChunks: {},
+        assetFileNames: "assets/[name].[hash][extname]",
+        chunkFileNames: "assets/[name].[hash].js",
+        entryFileNames: "assets/[name].[hash].js",
       },
     },
   },
